d2106210c2cb0cb199519f88b3ae0bd97470ca2b
MillaLaurinmaki <milla.laurinmaki@gmail.com>
2022-10-07 10:30:07 +0300

    changed api url & tried to fix the shoppingCart code

--- frontend/src/shoppingCartUtils.js
+++ frontend/src/shoppingCartUtils.js
@@ -1,15 +1,19 @@
-// Takes a list of fruit objects as arguments
-// eg. [{'name': 'Banana', 'price': 1}, {'name': 'Apple', 'price': 2}]
-// Should return the cart total.
-function calculateTotal (items) {
-  return 9999;
+function calculateTotal(items) {
+  let totalPrice = 0;
+  for (const item of items) {
+    totalPrice += item.price;
+  }
+  return totalPrice;
 }
+calculateTotal();
 
 // Takes a list of fruit objects as arguments
 // eg. [{'name': 'Banana', 'price': 1}, {'name': 'Apple', 'price': 2}]
 // Should return the cart average.
-function calculateAvg (items) {
-  return 9999;
+function calculateAvg(items) {
+  const average = totalPrice / items.price.length;
+  return average;
 }
+calculateAvg();
 
 export { calculateTotal, calculateAvg };
